a002848986c9b10e02e0ecfa778b6592bd5222c1
Anthony larez <ajlarezm@gmail.com>
2025-05-13 14:54:43 -0400

    post list lista

--- libros/urls.py
+++ libros/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path # importar la funcion path para crear la URL
-from libros.views import BookCreateView # importar la vista de creación de libros
+from libros.views import BookCreateView, PostListView # importar las vistas necesarias
 
 urlpatterns = [
+    path('post', PostListView.as_view(), name='post_list'), # URL para la lista de libros
     path('create/', BookCreateView.as_view(), name='post_create'), # URL para crear un nuevo libro
+
 ]
--- libros/views.py
+++ libros/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView # impartar la vista de creación
+from django.views.generic import CreateView, ListView# importar las vistas de creación y lista
 from .models import Book # importar el modelo de libro
 
 # Create your views here.
@@ -10,3 +10,8 @@ class BookCreateView(CreateView):
     # modelo para la creación de libros
     model = Book
     fields = ['titulo', 'autor', 'fecha_publicacion', 'paginas', 'idioma', 'image']  # campos del formulario
+
+class PostListView(ListView):
+        template_name = 'post_list.html'
+        model = Book  
+        context_object_name = 'lista_de_objects' #cambia el nombre de la lista de objetos
